4e6a8db7bdfda3ac63de68f27b84a712d130fefe
G-Mustafa <ghulammustafa137@gmail.com>
2019-07-23 01:30:05 +0500

    Updates

---
+++ precache-manifest.091069ca67762e815d7536e23c718cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a867633bcad2b0445e8af9653511c439",
+    "url": "/face-detection-app/index.html"
+  },
+  {
+    "revision": "02fa0a6d904b858bd81e",
+    "url": "/face-detection-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6beb93b5bd83eea19f5d",
+    "url": "/face-detection-app/static/css/main.143ca876.chunk.css"
+  },
+  {
+    "revision": "02fa0a6d904b858bd81e",
+    "url": "/face-detection-app/static/js/2.7ff1ac77.chunk.js"
+  },
+  {
+    "revision": "6beb93b5bd83eea19f5d",
+    "url": "/face-detection-app/static/js/main.c937ba52.chunk.js"
+  },
+  {
+    "revision": "2cc03c80c8f5fe75b687",
+    "url": "/face-detection-app/static/js/runtime~main.a1605b1a.js"
+  }
+]);
